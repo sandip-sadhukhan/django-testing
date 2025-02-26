e209b681f157833c6d1c30d4cea6bb92afb47c47
Sandip Sadhukhan <sandippakhanna@gmail.com>
2025-02-26 18:39:54 +0530

    test with mock

--- testing_project/products/tests/test_views.py
+++ testing_project/products/tests/test_views.py
@@ -1,6 +1,41 @@
 from django.test import TestCase, SimpleTestCase
 from products.models import Product, User
 from django.urls import reverse
+from unittest.mock import patch
+import requests
+
+
+class PostsViewTest(TestCase):
+    @patch('products.views.requests.get')
+    def test_post_view_success(self, mock_get):
+        mock_get.return_value.status_code = 200
+        return_data = {
+            "userId": 1,
+            "id": 1,
+            "title": "Test Title",
+            "body": "Test Body"
+        }
+        mock_get.return_value.json.return_value = return_data
+
+        response = self.client.get(reverse("post"))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, return_data)
+
+        mock_get.assert_called_once_with("https://jsonplaceholder.typicode.com/posts/1")
+
+    @patch('products.views.requests.get')
+    def test_post_view_fail(self, mock_get):
+        mock_get.side_effect = requests.exceptions.RequestException
+
+        response = self.client.get(reverse("post"))
+
+        self.assertEqual(response.status_code, 503)
+
+        mock_get.assert_called_once_with("https://jsonplaceholder.typicode.com/posts/1")
+
+
+
+
 
 class TestProfilePage(TestCase):
     def test_profile_view_redirects_for_anonymous_users(self):
--- testing_project/products/views.py
+++ testing_project/products/views.py
@@ -1,7 +1,11 @@
+import requests
+from requests.exceptions import RequestException
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse, HttpResponse
 from products.models import Product
 from products.forms import ProductForm
+
 
 
 def homePage(request):
@@ -32,3 +36,11 @@ def profile(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def post(request):
+    try:
+        response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+        response.raise_for_status()
+        return JsonResponse(response.json())
+    except RequestException as e:
+        return HttpResponse("Service unavailable", status=503)

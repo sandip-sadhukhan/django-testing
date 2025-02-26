fbbeb6beb15e03e685194f4bd9d0f76fd77df3d8
Sandip Sadhukhan <sandippakhanna@gmail.com>
2025-02-26 17:30:27 +0530

    refactor and add forms

--- testing_project/products/views.py
+++ testing_project/products/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect
+from products.models import Product
+from products.forms import ProductForm
+
 
 def homePage(request):
     return render(request, 'index.html')
 
 def products(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('products')
+        else:
+            context = {
+                'products': Product.objects.all(),
+                'form': form
+            }
+            return render(request, 'products.html', context)
+
+
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'form': ProductForm()}
     return render(request, 'products.html', context)
